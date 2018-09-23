90d35be35d91cdd1eeebebbe970cbc6ab3ba6d08
Aaron Lucia <engineal@users.noreply.github.com>
2018-09-22 20:36:36 -0400

    Exception handler additions

--- datapack/engineal/data/firmware/functions/exception.mcfunction
+++ datapack/engineal/data/firmware/functions/exception.mcfunction
@@ -1,16 +1,37 @@
+# mfc0  $k0, $14
 scoreboard players set address mem -2147483264
 scoreboard players set value mem 1075474432
 function asm:mem/write
+# addiu $k0, $k0, 4
 scoreboard players set address mem -2147483260
 scoreboard players set value mem 660209668
 function asm:mem/write
+# mtc0  $k0, $14
 scoreboard players set address mem -2147483256
 scoreboard players set value mem 1083863040
 function asm:mem/write
+# mtc0  $0, $13
 scoreboard players set address mem -2147483252
 scoreboard players set value mem 1082157056
 function asm:mem/write
+# mfc0  $k0, $12
 scoreboard players set address mem -2147483248
+scoreboard players set value mem 1075470336
+function asm:mem/write
+# andi  $k0, $k0, 0xfffd
+scoreboard players set address mem -2147483244
+scoreboard players set value mem 861601789
+function asm:mem/write
+# ori   $k0, $k0, 0x1
+scoreboard players set address mem -2147483240
+scoreboard players set value mem 928645121
+function asm:mem/write
+# mtc0  $k0, $12
+scoreboard players set address mem -2147483236
+scoreboard players set value mem 1083858944
+function asm:mem/write
+# eret
+scoreboard players set address mem -2147483232
 scoreboard players set value mem 1107296280
 function asm:mem/write
 tellraw @p {"text":"Installed exception.o!","color":"green"}
--- firmware/src/exception.asm
+++ firmware/src/exception.asm
@@ -1,10 +1,16 @@
 # Trap handler in the standard MIPS32 kernel text segment
 .ktext 0x80000180
-    mfc0  $k0, $14      # Bump EPC                  # 010000 00000 11010 01110 00000000 000 # ‭0x401A7000
-    addiu $k0, $k0, 4   # Do not re-execute         # 001001 11010 11010 0000000000000100   # 0x‭275A0004‬
-                        # faulting instruction
-    mtc0  $k0, $14      # EPC                       # 010000 00100 11010 01110 00000000 000 # ‭0x409A7000
+done:
+    mfc0  $k0, $14          # Bump EPC                  # 010000 00000 11010 01110 00000000 000 # ‭0x401A7000
+    addiu $k0, $k0, 4       # Do not re-execute         # 001001 11010 11010 0000000000000100   # 0x‭275A0004‬
+                            # faulting instruction
+    mtc0  $k0, $14          # EPC                       # 010000 00100 11010 01110 00000000 000 # ‭0x409A7000
     
-    mtc0  $0, $13       # Clear Cause register      # 010000 00100 00000 01101 00000000 000 # 0x‭40806800
+    mtc0  $0, $13           # Clear Cause register      # 010000 00100 00000 01101 00000000 000 # 0x‭40806800
     
-    eret                # Return to EPC             # 010000 1 0000000000000000000 011000   # 0x42000018
+    mfc0  $k0, $12          # Fix Status register       # 010000 00000 11010 01100 00000000 000 # 0x‭401A6000
+    andi  $k0, $k0, 0xfffd  # Clear EXL bit             # 001100 11010 11010 1111111111111101   # 0x‭335AFFFD
+    ori   $k0, $k0, 0x1     # Enable interrupts         # 001101 11010 11010 0000000000000001   # 0x‭375A0001
+    mtc0  $k0, $12                                      # 010000 00100 11010 01100 00000000 000 # 0x‭409A6000
+    
+    eret                    # Return to EPC             # 010000 1 0000000000000000000 011000   # 0x42000018
